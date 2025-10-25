6ef813d065abff965be53952cda9cff2d86e8673
Axelgk10 <axgimene@student.42malaga.com>
2025-10-25 02:43:49 +0200

    Fix formatting and include header in main.c

--- main.c
+++ main.c
@@ -1,23 +1,22 @@
-int main(argc int, argv **char)
-{
-    t_data  data;
+#include "philo.h"
 
-    if (argc != 5 && argc != 6)
-    {
-        printf("Usage: %s number_of_philosophers time_to_die ", argv[0]);
+int	main(int argc, char **argv)
+{
+	t_data	data;
+
+	if (argc != 5 && argc != 6)
+	{
+		printf("Usage: %s number_of_philosophers time_to_die ", argv[0]);
 		printf("time_to_eat time_to_sleep ");
 		printf("[number_of_times_each_philosopher_must_eat]\n");
 		return (1);
-    }
-    if (parse_args(argc, argv, &data) != 0)
+	}
+	if (parse_args(argc, argv, &data) != 0)
 		return (1);
-	
 	if (init_data(&data) != 0)
 		return (1);
-	
 	if (start_simulation(&data) != 0)
 		return (1);
-	
 	cleanup(&data);
 	return (0);
 }
